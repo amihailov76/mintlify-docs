2673f6fd6a6da49f45299cf00556a3d98c019407
amihailov76 <amihailov76@gmail.com>
2026-05-03 17:19:25 +0300

    feat: add do11y analytics tracking

---
+++ scripts/do11y-config.js
@@ -0,0 +1,19 @@
+window.Do11yConfig = {
+  axiomHost:    'AXIOM_HOST_PLACEHOLDER',
+  axiomToken:   'AXIOM_TOKEN_PLACEHOLDER',
+  axiomDataset: 'mintlify-docs',
+
+  framework: 'mintlify',
+
+  allowedDomains: ['pt-85b9e7a5.mintlify.app'],
+
+  trackScrollDepth:        true,
+  trackSectionVisibility:  true,
+  trackTabSwitches:        true,
+  trackTocClicks:          true,
+  trackExpandCollapse:     true,
+  trackFeedback:           true,
+  sectionVisibleThreshold: 5,
+
+  respectDNT: true,
+};
